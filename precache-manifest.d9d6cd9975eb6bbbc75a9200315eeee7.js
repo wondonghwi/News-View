c6c2f3521e9d2785f36c41aa9e55adf36c6d4794
원동휘 <ehdehd1289@naver.com>
2020-03-08 23:09:28 +0900

    Updates

---
+++ precache-manifest.d9d6cd9975eb6bbbc75a9200315eeee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202eedd5c37a3228e2335397f4330ca4",
+    "url": "/News-View/index.html"
+  },
+  {
+    "revision": "4df0a011b10183760782",
+    "url": "/News-View/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "153867976c7dfc5a5e43",
+    "url": "/News-View/static/js/2.7069be3d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/News-View/static/js/2.7069be3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df0a011b10183760782",
+    "url": "/News-View/static/js/main.03995219.chunk.js"
+  },
+  {
+    "revision": "e4dec1ecd8bee246b349",
+    "url": "/News-View/static/js/runtime-main.6eab9fbf.js"
+  }
+]);
