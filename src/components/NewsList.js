2c645ceb2c9c4d28ca7c0ca04bfc5778e0768e41
donghwi <ehdehd1289@naver.com>
2021-01-11 23:27:17 +0900

    change api=key

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -21,7 +21,7 @@ const NewsList = ({ category }) => {
   const [loading, response, error] = usePromise(() => {
     const query = category === 'all' ? '' : `&category=${category}`;
     return axios.get(
-      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f`,
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=6c3459a8d75e4836a056e09bd19c9e79`,
     );
   }, [category]);
 
